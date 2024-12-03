6cab22874a725a2bbe415722d6b0219cbe6e4b47
yoonseoq <sysooo18@naver.com>
2024-12-04 01:50:26 +0900

    kjh

--- src/main/java/com/green/greengramver2/common/Constants.java
+++ src/main/java/com/green/greengramver2/common/Constants.java
@@ -1,11 +1,17 @@
 package com.green.greengramver2.common;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-@Component // 빈등록 spring container가 갹체생성
+// 빈등록 spring container가 갹체생성
 // @value yaml에 있는 데이터를 건네준다 이 애노테이션 붙는 순간 그 메소드 무조건 부름
 // 20값을 리턴할 수 있다.
+@Component
 public class Constants {
+
+    private static int default_page_size;
+
+    @Value("${const.default-page-size}")
 
 
 
