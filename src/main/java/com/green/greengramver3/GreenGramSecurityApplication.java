a81341f1ace2d40ba6c864ad15b207153105804b
yoonseoq <sysooo18@naver.com>
2024-12-16 10:54:26 +0900

    1210

--- src/main/java/com/green/greengramver3/GreenGramSecurityApplication.java
+++ src/main/java/com/green/greengramver3/GreenGramSecurityApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GreenGramVer3Application {
+public class GreenGramSecurityApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GreenGramVer3Application.class, args);
+        SpringApplication.run(GreenGramSecurityApplication.class, args);
     }
 
 }
